43130e0dfdb1e1747db458a056f50d5a398aa547
Lisfe00 <78762644+Lisfe00@users.noreply.github.com>
2022-12-07 10:44:03 -0300

    add script em  script.js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,9 @@ var nome = document.querySelector('#name');
 const texto = document.querySelector('#texto_modal');
 const texto_modal_body = document.querySelector("#texto_modal_body");
 
-function modal_open(){
+function modal_open(e){
 
+    
     console.log(nome);
     if(nome.value == ""){
         modal.style.display = "block";
@@ -25,3 +26,16 @@ function close_modal(){
     modal.style.display = "none";
     fade.style.display = "none";
 }
+
+//expandir
+
+    var secao = document.querySelector('.cursos-op2') 
+    var button = document.querySelector('.button-form') 
+    
+    button.addEventListener("click", expand);
+    
+    function expand() {
+        console.log(secao, button);
+        secao.style.display = "flex";
+        button.style.display = "none"
+    }
