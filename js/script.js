14b80bb2e28bb5bc8b69c42467dff16c05b37148
Lisfe00 <78762644+Lisfe00@users.noreply.github.com>
2022-12-08 00:33:32 -0300

    mascara de fone e verificação de form por js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 const modal = document.querySelector('#modal');
 const fade = document.querySelector('#fade');
 var nome = document.querySelector('#name');
+var email = document.querySelector('#email');
+var fone = document.querySelector('#numero');
+var senha = document.querySelector('#senha');
+var area = document.querySelector('#area');
 const texto = document.querySelector('#texto_modal');
 const texto_modal_body = document.querySelector("#texto_modal_body");
 
 function modal_open(e){
 
-    
+
     console.log(nome);
-    if(nome.value == ""){
+    if(nome.value == "" || email.value == "" || fone.value == "" || senha.value == "" || area.value == ""){
         modal.style.display = "block";
         fade.style.display = "block";  
         texto.innerHTML = "Digite seus dados no formulário";
@@ -39,3 +43,22 @@ function close_modal(){
         secao.style.display = "flex";
         button.style.display = "none"
     }
+
+
+//mascara de fone
+
+function mascara_fone() {
+    let nfone = fone.value.length;
+    console.log(nfone);
+  
+    if(nfone == 1){
+      fone.value = `(${fone.value}`
+    }else if(nfone == 3){
+      fone.value += ") "
+    }else if(nfone == 6){
+      fone.value += " "
+    }else if(nfone == 11){
+      fone.value += "-"
+    }
+    
+  }
